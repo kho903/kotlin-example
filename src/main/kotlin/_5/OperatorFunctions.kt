d3a19e2a2d10f6e95d9e4932e693dcc36751adf6
Kim Ji Hun <gmldnr2222@naver.com>
2023-08-23 05:03:24 +0900

    feat: 연산자 함수 Operator Functions

---
+++ src/main/kotlin/_5/OperatorFunctions.kt
@@ -0,0 +1,11 @@
+package _5
+
+fun main() {
+    operator fun Int.times(str: String) = str.repeat(this)
+    println(2 * "하이 ")
+    println(2.times("하이 "))
+
+    operator fun String.get(range: IntRange) = substring(range)
+    val str = "웃어라! 온 세상이 너와 함께 웃을 것이다."
+    println(str[0..3])
+}
