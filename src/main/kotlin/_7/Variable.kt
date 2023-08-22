f8e31b5a46c49a2555ec6d9e79facab3c6167a81
Kim Ji Hun <gmldnr2222@naver.com>
2023-08-23 05:34:50 +0900

    feat: 변수 Variable

---
+++ src/main/kotlin/_7/Variable.kt
@@ -0,0 +1,11 @@
+package _7
+
+fun main() {
+    var a: String = "처음값"
+    println(a)
+    a = "다른값"
+    println(a)
+    val b: Int = 1
+    val c = 3
+//    b = 2 // Kotlin: Val cannot be reassigned
+}
