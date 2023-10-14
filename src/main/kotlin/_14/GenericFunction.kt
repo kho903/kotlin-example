475b471845190f478bba143a4afb8947f5149a3b
kho903 <gmldnr2222@naver.com>
2023-10-14 16:43:18 +0900

    feat: 제네릭 타입
    - 다양한 타입에 공통된 코드 추상화
    - 공통 코드에서 불특정 타입을 두고, 그 외에 독립적인 공통 코드를 잘 감싸서 쓸 수 있게 해서 코드 재사용성을 높여줌.

---
+++ src/main/kotlin/_14/GenericFunction.kt
@@ -0,0 +1,10 @@
+package _14
+
+import _13.MutableStack
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+    val stack = mutableStackOf(0.62, 3.14, 2.7)
+    println(stack)
+}
